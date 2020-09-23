dce43053768bd60b07be0ecf6093f2de0cec74e3
le.zhang <le.zhang@silot.ai>
2020-09-23 09:17:16 +0800

    debug

--- detectron2/modeling/box_regression.py
+++ detectron2/modeling/box_regression.py
@@ -152,7 +152,7 @@ class Box2BoxTransform(object):
             area_t = 0
             for item in target_boxes_list[0]:
                 if box.equal(item):
-                    if (torch.gt(pred_class_logits[i][0], score_num)):
+                    if (torch.gt(score_num, pred_class_logits[i][0])):
                         score_num = pred_class_logits[i][0]
                         print('closed pre box: ', src_boxes_list[0][i])
                         area_t = (src_boxes_list[0][i][2] - src_boxes_list[0][i][0]) * (src_boxes_list[0][i][3] - item[1])
